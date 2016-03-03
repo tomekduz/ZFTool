07d7a7570f6ffe1cc45b7934a1e486c9cf87ac39
Witold Wasiczko <witold.wasiczko@rst.com.pl>
2016-03-03 11:10:59 +0100

    Add compatibility to zf service manager 3.0

--- src/ZFTool/Controller/DiagnosticsControllerFactory.php
+++ src/ZFTool/Controller/DiagnosticsControllerFactory.php
@@ -2,12 +2,16 @@
 
 namespace ZFTool\Controller;
 
+use Zend\Mvc\Controller\ControllerManager;
 use Zend\ServiceManager\ServiceLocatorInterface;
 
 class DiagnosticsControllerFactory
 {
     public function __invoke(ServiceLocatorInterface $serviceLocator)
     {
+        if ($serviceLocator instanceof ControllerManager && method_exists($serviceLocator, 'getServiceLocator')) {
+            $serviceLocator = $serviceLocator->getServiceLocator();
+        }
         return new DiagnosticsController($serviceLocator);
     }
 }
